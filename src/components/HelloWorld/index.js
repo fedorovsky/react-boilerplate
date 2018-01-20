a27548eae49ca05bbc6f3648c01f203ed7772ee8
Fedorovskyi <anton.fedorovskyi@gmail.com>
2018-01-20 18:00:02 +0200

    add css modules

--- src/components/HelloWorld/index.js
+++ src/components/HelloWorld/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './index.css';
+import styles from './index.css';
 
 const HelloWorld = (props) => {
     return (
-        <div className="parent">
-            <h1 className="child">Hello World Component</h1>
+        <div className={styles.parent}>
+            <h1 className={styles.child}>Hello World Component</h1>
         </div>
     )
 }
